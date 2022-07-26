b0288327d906f579b040cf0b2d136a5f3a9975c6
jfaboytesh <jfaboytesh@gmail.com>
2022-07-25 20:04:36 -0500

    Define message variables

---
+++ script.js
@@ -0,0 +1,17 @@
+const greetingMessage = [
+    ['I know you are tired and want to give up, '],
+    ['You should be proud of yourself, '],
+    ["It doesn't matter if you logged just for one hour, "]
+];
+
+const middleMessage = [
+    ['some things take longer than you think, but here you are! '],
+    ["There's no regrets in your life, you are doing what you want! "],
+    ["It's not easy, either hard! "]
+];
+
+const goodbyeMessage = [
+    ['You are learning to code, and you will have the life that you want!'],
+    ['You are learning to code, so never give up!'],
+    ['You are learning to code, rest and take a break, but never quit!']
+]
