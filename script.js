f1e9eba759668f0d828513b0af27b18b3f37f6c9
jfaboytesh <jfaboytesh@gmail.com>
2022-11-17 16:11:35 -0600

    Redefine UI with HTML and CSS

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const greetingMessage = [
-    ['I know you are tired and want to give up,'],
-    ['You should be proud of yourself,'],
-    ["It doesn't matter if you logged just for one hour,"]
+  ['I know you are tired and want to give up,'],
+  ['You should be proud of yourself,'],
+  ["It doesn't matter if you logged just for one hour,"]
 ];
 
 const middleMessage = [
-    ['some things take longer than you think, but here you are!'],
-    ["there's no regrets in your life, you are doing what you want!"],
-    ["it's not easy, either hard!"]
+  ['some things take longer than you think, but here you are!'],
+  ["there's no regrets in your life, you are doing what you want!"],
+  ["it's not easy, either hard!"]
 ];
 
 const goodbyeMessage = [
-    ['You are learning to code, and you will have the life that you want!'],
-    ['You are learning to code, so never give up!'],
-    ['You are learning to code, rest and take a break, but never quit!']
+  ['You are learning to code, and you will have the life that you want!'],
+  ['You are learning to code, so never give up!'],
+  ['You are learning to code, rest and take a break, but never quit!']
 ];
 
 const biggestArray = (greeting, middle, goodbye) => {
-    let maxValue = 0;
-    if(greeting.length > middle.length) {
-        maxValue = greeting.length;
-    }
-    else {
-        maxValue = middle.length;
-    }
-    if(goodbye.length > maxValue) {
-        maxValue = goodbye.length;
-    }
-    return maxValue;
+  let maxValue = 0;
+  if(greeting.length > middle.length) {
+    maxValue = greeting.length;
+  }
+  else {
+    maxValue = middle.length;
+  }
+  if(goodbye.length > maxValue) {
+    maxValue = goodbye.length;
+  }
+  return maxValue;
 }
 
 const len = biggestArray(greetingMessage, middleMessage, goodbyeMessage);
@@ -36,16 +36,16 @@ const random = () => Math.floor(Math.random() * len);
 const finalMessage = [];
 
 const randomPhrase = phrase => {
-    switch(random()) {
-        case 0:
-            finalMessage.push(phrase[0]);
-            break;
-        case 1: 
-            finalMessage.push(phrase[1]);
-            break;
-        case 2:
-            finalMessage.push(phrase[2]);
-            break;
+  switch(random()) {
+    case 0:
+      finalMessage.push(phrase[0]);
+      break;
+    case 1: 
+      finalMessage.push(phrase[1]);
+      break;
+    case 2:
+      finalMessage.push(phrase[2]);
+      break;
     }
     return finalMessage;
 }
@@ -60,7 +60,7 @@ const motivationQuote = document.querySelector('.motivation');
 motivationQuote.innerHTML = phrases;
 
 const visible = () => {
-    motivationQuote.style.visibility = 'visible';
+  motivationQuote.style.visibility = 'visible';
 }
 
 document.getElementById('mquote').addEventListener('click', visible);
