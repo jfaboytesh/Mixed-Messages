83067d82e4862703a707e0ba9e76574845deb01b
jfaboytesh <jfaboytesh@gmail.com>
2022-11-17 18:18:09 -0600

    Handle interactions with the DOM

--- readme.md
+++ readme.md
@@ -2,8 +2,12 @@
 
 First of all, if anyone is reading this, thank you in advance. This is my very **first portfolio proyect**, and I know it may not be the greatest, but so far I have been learning to code for about *two months (9 weeks or 71 days)*.
 
-## Version 2.0
+## Version 1.1
 
 I added HTML and CSS to this project to practice everything I know so far.
 
-> First version: 26 of July 2022 | Second version: 03 of August 2022
+## Version 1.2
+
+I **fully remake the HTML and CSS** in this project. Also, I modified the JavaScript interactions with the DOM and let the buttom repeat a random phrase.
+
+> Version 1.0: 26 of July 2022, Version 1.1: 03 of August 2022, Version 1.2: 17 of November 2022.
--- script.js
+++ script.js
@@ -1,120 +1,67 @@
-const greetingMessage = [
-  ['I know you are tired and want to give up,'],
-  ['You should be proud of yourself,'],
-  ["It doesn't matter if you logged just for one hour,"]
-];
+const handlePhrase = () => {
 
-const middleMessage = [
-  ['some things take longer than you think, but here you are!'],
-  ["there's no regrets in your life, you are doing what you want!"],
-  ["it's not easy, either hard!"]
-];
+  const greetingMessage = [
+    ['I know you are tired and want to give up,'],
+    ['You should be proud of yourself,'],
+    ["It doesn't matter if you logged just for one hour,"]
+  ];
 
-const goodbyeMessage = [
-  ['You are learning to code, and you will have the life that you want!'],
-  ['You are learning to code, so never give up!'],
-  ['You are learning to code, rest and take a break, but never quit!']
-];
+  const middleMessage = [
+    ['some things take longer than you think, but here you are!'],
+    ["there's no regrets in your life, you are doing what you want!"],
+    ["it's not easy, either hard!"]
+  ];
 
-const biggestArray = (greeting, middle, goodbye) => {
-  let maxValue = 0;
-  if(greeting.length > middle.length) {
-    maxValue = greeting.length;
-  }
-  else {
-    maxValue = middle.length;
-  }
-  if(goodbye.length > maxValue) {
-    maxValue = goodbye.length;
-  }
-  return maxValue;
-}
+  const goodbyeMessage = [
+    ['You are learning to code, and you will have the life that you want!'],
+    ['You are learning to code, so never give up!'],
+    ['You are learning to code, rest and take a break, but never quit!']
+  ];
 
-const len = biggestArray(greetingMessage, middleMessage, goodbyeMessage);
-const random = () => Math.floor(Math.random() * len);
-
-const finalMessage = [];
-
-const randomPhrase = phrase => {
-  switch(random()) {
-    case 0:
-      finalMessage.push(phrase[0]);
-      break;
-    case 1: 
-      finalMessage.push(phrase[1]);
-      break;
-    case 2:
-      finalMessage.push(phrase[2]);
-      break;
+  const biggestArray = (greeting, middle, goodbye) => {
+    let maxValue = 0;
+    if(greeting.length > middle.length) {
+      maxValue = greeting.length;
     }
-    return finalMessage;
-}
+    else {
+      maxValue = middle.length;
+    }
+    if(goodbye.length > maxValue) {
+      maxValue = goodbye.length;
+    }
+    return maxValue;
+  }
 
-randomPhrase(greetingMessage);
-randomPhrase(middleMessage);
-randomPhrase(goodbyeMessage);
+  const len = biggestArray(greetingMessage, middleMessage, goodbyeMessage);
+  const random = () => Math.floor(Math.random() * len);
 
-const phrases = finalMessage.join(' ');
+  const finalMessage = [];
 
-const motivationQuote = document.querySelector('.motivation');
-motivationQuote.innerHTML = phrases;
+  const randomPhrase = phrase => {
+    switch(random()) {
+      case 0:
+        finalMessage.push(phrase[0]);
+        break;
+      case 1: 
+        finalMessage.push(phrase[1]);
+        break;
+      case 2:
+        finalMessage.push(phrase[2]);
+        break;
+      }
+      return finalMessage;
+  }
 
-const visible = () => {
+  randomPhrase(greetingMessage);
+  randomPhrase(middleMessage);
+  randomPhrase(goodbyeMessage);
+
+  const phrases = finalMessage.join(' ');
+
+  const motivationQuote = document.querySelector('.motivation');
+  motivationQuote.innerHTML = phrases;
   motivationQuote.style.visibility = 'visible';
+  
 }
 
-document.getElementById('mquote').addEventListener('click', visible);
-
-/*const randomGreeting = greeting => {
-    const random = Math.floor(Math.random() * 3);
-    switch(random) {
-        case 0:
-            finalMessage.push(greeting[0]);
-            break;
-        case 1:
-            finalMessage.push(greeting[1]);
-            break;
-        case 2:
-            finalMessage.push(greeting[2]);
-            break;
-    }
-    return finalMessage;
-}
-
-const randomMiddle = middle => {
-    const random = Math.floor(Math.random() * 3);
-    switch(random) {
-        case 0:
-            finalMessage.push(middle[0]);
-            break;
-        case 1:
-            finalMessage.push(middle[1]);
-            break;
-        case 2:
-            finalMessage.push(middle[2]);
-            break;
-    }
-    return finalMessage;
-}
-
-const randomGoodbye = goodbye => {
-    const random = Math.floor(Math.random() * 3);
-    switch(random) {
-        case 0:
-            finalMessage.push(goodbye[0]);
-            break;
-        case 1:
-            finalMessage.push(goodbye[1]);
-            break;
-        case 2:
-            finalMessage.push(goodbye[2]);
-            break;
-    }
-    return finalMessage;
-}
-
-randomGreeting(greetingMessage);
-randomMiddle(middleMessage);
-randomGoodbye(goodbyeMessage)
-
-console.log(finalMessage.join(' '));*/
+document.querySelector('.mquote').addEventListener('click', handlePhrase);
